00c67fa07709db39ad37a64f29dacfe6064729bc
Ludwig Horvath <65821757+JELH8980@users.noreply.github.com>
2024-01-25 11:32:56 +0100

    Update B2physical_frame.m

--- Functions/B2physical_frame.m
+++ Functions/B2physical_frame.m
@@ -7,8 +7,8 @@ function   [a1, a2] = B2physical_frame(FrameB)
          sin(pi/4),  cos(pi/4), 0;
          0        ,  0        , 1];
 
-    a1 = R'*FrameB*[1;0;0];
+    a1 = FrameBR'[1;0;0];
 
-    a2 = R*FrameB*[1;0;0];
+    a2 = FrameBR[1;0;0];
 
 end
